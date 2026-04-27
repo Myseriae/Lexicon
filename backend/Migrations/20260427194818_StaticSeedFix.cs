f4f44b917ff16d614e8c779b251427f2d3163c29
Szabó Domonkos <szdomkacs@gmail.com>
2026-04-27 22:24:30 +0200

    fix/migration

---
+++ backend/Migrations/20260427194818_StaticSeedFix.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lexicon.Migrations
+{
+    /// <inheritdoc />
+    public partial class StaticSeedFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2026, 4, 27, 15, 43, 24, 467, DateTimeKind.Local).AddTicks(2560));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2026, 4, 27, 15, 43, 24, 480, DateTimeKind.Local).AddTicks(6620));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2026, 4, 27, 15, 43, 24, 480, DateTimeKind.Local).AddTicks(6630));
+        }
+    }
+}
