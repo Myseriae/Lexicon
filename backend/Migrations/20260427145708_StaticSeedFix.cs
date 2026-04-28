96f3c77f7838adeb39073999c5da8d3e0e2ea079
Zsolt Vajna <zsoleex@gmail.com>
2026-04-28 09:04:12 +0200

    small changes in DbContext
    # Conflicts:
    #	backend/Data/LexiconDbContext.cs
    #	backend/Migrations/LexiconDbContextModelSnapshot.cs

---
+++ backend/Migrations/20260427145708_StaticSeedFix.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Lexicon.Migrations
+{
+    /// <inheritdoc />
+    public partial class StaticSeedFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2026, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2026, 4, 27, 15, 43, 24, 467, DateTimeKind.Local).AddTicks(2560));
+
+            migrationBuilder.InsertData(
+                table: "Articles",
+                columns: new[] { "Id", "Content", "Created", "Title" },
+                values: new object[,]
+                {
+                    { 2, "You use it to build a web application.", new DateTime(2026, 4, 27, 15, 43, 24, 480, DateTimeKind.Local).AddTicks(6620), "ASP.NET Core" },
+                    { 3, "Used to build RESTful APIs.", new DateTime(2026, 4, 27, 15, 43, 24, 480, DateTimeKind.Local).AddTicks(6630), "REST API" }
+                });
+        }
+    }
+}
