fb2de256d79f26e712753963dac906a1562012e6
Szabó Domonkos <szdomkacs@gmail.com>
2026-04-28 14:15:37 +0200

    add/Explicit ArticleId in Revision

--- backend/Model/Revision.cs
+++ backend/Model/Revision.cs
@@ -6,5 +6,6 @@ public class Revision
     public string Content { get; set; } = "";
     public DateTime SavedAt { get; set; } = DateTime.Now;
     public int VersionNumber { get; set; }
+    public int ArticleId { get; set; }
     public Article Article { get; set; } = null!;
 }
